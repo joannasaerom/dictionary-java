c43934462090e32a8d399bd9de70c67323cf27eb
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-09-16 10:36:46 -0700

    Adds styling to dictionary

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,57 +1,57 @@
-// import java.util.HashMap;
-// import java.util.Map;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
-// import java.util.List;
-// import java.util.ArrayList;
-//
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//     String layout = "template/layout.vtl";
-//
-//     get("/", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("template", "template/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     post("/words", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       String addWord = request.queryParams("addWord");
-//       Word word = new Word(addWord);
-//       model.put("words", Word.all());
-//       model.put("template", "template/dictionary.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/words", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("words", Word.all());
-//       model.put("template", "template/dictionary.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/words/:id", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       Word word = Word.find(Integer.parseInt(request.params(":id")));
-//       model.put("definitions", word.getDefinitions());
-//       model.put("word", word);
-//       model.put("template", "template/detail.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     post("/words/:id", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       Word word = Word.find(Integer.parseInt(request.params(":id")));
-//       String definition = request.queryParams("addDefinition");
-//       Definition newDefinition = new Definition(definition);
-//       word.addDefinition(newDefinition);
-//       model.put("definitions", word.getDefinitions());
-//       model.put("word", word);
-//       model.put("template", "template/detail.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//   }
-// }
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "template/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "template/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String addWord = request.queryParams("addWord");
+      Word word = new Word(addWord);
+      model.put("words", Word.all());
+      model.put("template", "template/dictionary.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "template/dictionary.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("definitions", word.getDefinitions());
+      model.put("word", word);
+      model.put("template", "template/detail.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      String definition = request.queryParams("addDefinition");
+      Definition newDefinition = new Definition(definition);
+      word.addDefinition(newDefinition);
+      model.put("definitions", word.getDefinitions());
+      model.put("word", word);
+      model.put("template", "template/detail.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
